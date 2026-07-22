f60971e176258115eee969e7800f900655574a19
ricpaula <cadodepaulasilva159@gmail.com>
2026-07-22 16:02:07 -0300

    Update cache app

--- main.py
+++ main.py
@@ -4,5 +4,12 @@ app = Flask(__name__)
 
 from views import *
 
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '0'
+    return response
+
 if __name__ == "__main__":
     app.run()
